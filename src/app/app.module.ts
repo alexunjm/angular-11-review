8109e75ced31a50f3b4216a78d4c8fd6048a0941
alexanderjaramillo <alexunjm@gmail.com>
2020-12-13 14:55:59 +0000

    generación básica de directiva - se realiza ejemplo con directiva directive-example - se declara en los módulos de app la directiva (igual que con componentes)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { DirectiveExampleDirective } from './directive-example.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    DirectiveExampleDirective
   ],
   imports: [
     BrowserModule,
