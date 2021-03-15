e615a924e3481859dceb89a41d3a7df9f9ab827f
alexanderjaramillo <contacto@alexanderjaramillo.com>
2021-03-15 16:12:54 -0500

    input reactivo utilizando FormControl

--- src/app/my-reactive-forms/components/first-form/first-form.component.ts
+++ src/app/my-reactive-forms/components/first-form/first-form.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-first-form',
   templateUrl: './first-form.component.html',
-  styleUrls: ['./first-form.component.scss']
+  styleUrls: ['./first-form.component.scss'],
 })
 export class FirstFormComponent implements OnInit {
+  name = new FormControl('');
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
