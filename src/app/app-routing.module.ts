d762f00dcfdba39062ab33fc6860394d54100d74
alexanderjaramillo <contacto@alexanderjaramillo.com>
2021-04-17 08:36:47 -0500

    reordenamiento de rutas, módulos y componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { FirstComponent } from './components/first/first.component';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-import { SecondComponent } from './components/second/second.component';
+import { FirstComponent } from './first/first.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SecondComponent } from './second/second.component';
 
 const routes: Routes = [
   { path: 'first', component: FirstComponent },
