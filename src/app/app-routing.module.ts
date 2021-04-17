f4f5fe9087e8688a60b00dd9ee7ab16ab298f945
alexanderjaramillo <contacto@alexanderjaramillo.com>
2021-04-17 07:57:23 -0500

    página no encontrada - se agrega un ejemplo de un wildcard genérico, para atrapar rutas que no existen y mostrar una página específica para ello

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FirstComponent } from './components/first/first.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { SecondComponent } from './components/second/second.component';
 
 const routes: Routes = [
   { path: 'first', component: FirstComponent },
   { path: 'second', component: SecondComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FirstComponent } from './components/first/first.component';
-import { SecondComponent } from './components/second/second.component';
 
 @NgModule({
   declarations: [AppComponent],
