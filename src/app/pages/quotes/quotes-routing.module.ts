39883b99711cbc7fdf8f45313196c58c61ecf05f
alexanderjaramillo <contacto@alexanderjaramillo.com>
2021-05-08 11:32:19 -0500

    default redirect on quotes

--- src/app/pages/quotes/quotes-routing.module.ts
+++ src/app/pages/quotes/quotes-routing.module.ts
@@ -29,7 +29,7 @@ const routes: Routes = [
             (m) => m.CharacterModule
           ),
       },
-      // { path: '**', redirectTo: 'random' },
+      { path: '**', redirectTo: 'random' },
     ],
   },
 ];
