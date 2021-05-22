6eb60f398778edeb7fdfd0259576beadecab44d9
alexanderjaramillo <contacto@alexanderjaramillo.com>
2021-05-22 03:39:10 -0500

    input example

--- src/app/pages/forms/template-driven/example1/example1.component.ts
+++ src/app/pages/forms/template-driven/example1/example1.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../model/user';
 
 @Component({
   selector: 'app-example1',
   templateUrl: './example1.component.html',
-  styleUrls: ['./example1.component.scss']
+  styleUrls: ['./example1.component.scss'],
 })
 export class Example1Component implements OnInit {
+  roles = ['SysAdmin', 'Teacher', 'Student'];
 
-  constructor() { }
+  user!: User;
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
