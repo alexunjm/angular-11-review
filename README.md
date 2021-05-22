5207aa143b6326d3822cdf2c12298b2b92870bb7
alexanderjaramillo <contacto@alexanderjaramillo.com>
2021-05-22 03:19:23 -0500

    reactie forms example2 component

--- README.md
+++ README.md
@@ -3,5 +3,9 @@
 ## Commands
 
     ng g m pages/forms -m app --route forms
+
     ng g m pages/forms/template-driven
     ng g c pages/forms/template-driven/example1 -m pages/forms/template-driven
+
+    ng g m pages/forms/reactive-forms
+    ng g c pages/forms/reactive-forms/example2 -m pages/forms/reactive-forms
--- src/app/pages/forms/forms.module.ts
+++ src/app/pages/forms/forms.module.ts
@@ -4,9 +4,15 @@ import { CommonModule } from '@angular/common';
 import { FormsRoutingModule } from './forms-routing.module';
 import { FormsComponent } from './forms.component';
 import { TemplateDrivenModule } from './template-driven/template-driven.module';
+import { ReactiveFormsModule } from './reactive-forms/reactive-forms.module';
 
 @NgModule({
   declarations: [FormsComponent],
-  imports: [CommonModule, FormsRoutingModule, TemplateDrivenModule],
+  imports: [
+    CommonModule,
+    FormsRoutingModule,
+    TemplateDrivenModule,
+    ReactiveFormsModule,
+  ],
 })
 export class FormsModule {}
