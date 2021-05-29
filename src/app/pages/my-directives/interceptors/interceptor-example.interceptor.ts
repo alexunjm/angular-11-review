cef31538c2b8fa2d3f7194726a1551e0eed1eae3
alexanderjaramillo <contacto@alexanderjaramillo.com>
2021-05-29 06:35:53 -0500

    manejo/log de errores

--- src/app/pages/my-directives/interceptors/interceptor-example.interceptor.ts
+++ src/app/pages/my-directives/interceptors/interceptor-example.interceptor.ts
@@ -6,27 +6,35 @@ import {
   HttpInterceptor,
   HttpEventType,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 
 @Injectable()
 export class InterceptorExampleInterceptor implements HttpInterceptor {
   constructor() {}
 
   intercept(
-    request: HttpRequest<unknown>,
+    request: HttpRequest<any>,
     next: HttpHandler
-  ): Observable<HttpEvent<unknown>> {
+  ): Observable<HttpEvent<any>> {
     console.log(`consumiendo ${request.url}`);
 
     const modifiedRequest = request.clone({
       headers: request.headers.append('Auth', 'abc'),
     });
+
     return next.handle(modifiedRequest).pipe(
       tap((event) => {
         console.log({ [request.url]: { event } });
         if (event.type === HttpEventType.Response) {
           console.log({ body: event.body });
+        }
+      }),
+      catchError((error) => {
+        switch (error.status) {
+          default:
+            console.dir(error);
+            return throwError(error);
         }
       })
     );
